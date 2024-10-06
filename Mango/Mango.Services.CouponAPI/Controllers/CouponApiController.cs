dc833dd429cc2dca3f62b9d523ae242fb0a3d08d
Lushen Moodley <lushenmoodley@yahoo.com>
2024-10-06 11:27:28 +0200

    set project structure

---
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponApiController.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponApiController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponApiController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+    }
+}
